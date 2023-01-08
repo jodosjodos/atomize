c0cbefd0c789861c7698bfb5634abed53e99f948
jodosjodos <jeandedieu2030@gmail.com>
2023-01-08 19:05:00 +0200

    adding student service

---
+++ student-service/src/main/java/com/atomize/StudentService.java
@@ -0,0 +1,14 @@
+package com.atomize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//student service
+@SpringBootApplication
+public class StudentService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentService.class, args);
+    }
+
+}
