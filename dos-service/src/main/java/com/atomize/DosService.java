70dec5cd9412f6b30354e58da9f438ed2fbc690a
jodosjodos <jeandedieu2030@gmail.com>
2024-01-05 22:28:15 +0200

    initializing project

---
+++ dos-service/src/main/java/com/atomize/DosService.java
@@ -0,0 +1,13 @@
+package com.atomize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DosService.class, args);
+    }
+
+}
---
+++ src/test/java/com/atomize/atomize/AtomizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atomize.atomize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
