0d5b1905399aa4983fb4cdab36cfb98d79294464
jodosjodos <jeandedieu2030@gmail.com>
2024-01-08 11:47:35 +0200

    remove some bugs

--- dos-service/src/main/java/com/atomize/dtos/SignUpRequest.java
+++ dos-service/src/main/java/com/atomize/dtos/SignUpRequest.java
@@ -1,6 +1,3 @@
 package com.atomize.dtos;
-
-import lombok.RequiredArgsConstructor;
-
 public record SignUpRequest(String name, String email, String password, String schoolName, String phoneNumber) {
 }
