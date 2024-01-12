f225fce8ed042aa10e24f227016fa94140d9e158
jodosjodos <jeandedieu2030@gmail.com>
2024-01-12 07:07:25 +0200

    fix bug1

--- dos-service/src/main/java/com/atomize/Controller/DosController.java
+++ dos-service/src/main/java/com/atomize/Controller/DosController.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
+// dos controller
 @RestController
 @RequestMapping("/api/dos")
 @RequiredArgsConstructor
