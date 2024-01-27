550ea3ea774ed130d89fd82e9c4950b88c3dc9eb
jodosjodos <jeandedieu2030@gmail.com>
2024-01-27 14:27:05 +0200

    initialize all  services

--- subject-service/src/main/java/com/atomize/DosService.java
+++ subject-service/src/main/java/com/atomize/DosService.java
@@ -2,12 +2,12 @@ package com.atomize;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//student service
+
 @SpringBootApplication
-public class StudentService {
+public class DosService {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudentService.class, args);
+        SpringApplication.run(DosService.class, args);
     }
 
 }
